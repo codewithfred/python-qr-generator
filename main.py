7145b0a854f37965506ba8030044b85da980bdd1
Alfred <alfred.verihubs@gmail.com>
2021-06-22 20:32:14 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import qrcode
+
+input_data = "https://instagram.com/codewithfred"
+
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+qr.add_data(input_data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+img.save('qr-result.png')
